09aad8a2cf79340aa2cff222f2162ad2b7ee2683
Sean Price <seanprice@sevenhillstechnology.com>
2020-01-06 10:27:17 -0500

    Footer layout changes

--- src/internal-components/PagerFooter.tsx
+++ src/internal-components/PagerFooter.tsx
@@ -18,28 +18,31 @@ export const PagerFooter: React.FunctionComponent<ColumnHeaderRowProps> = props 
     return (
         <tfoot>
             <tr className="fancy-grid-footer-row">
-                <td className="fancy-grid-footer-controls" colSpan={props.columnDefinitions.length}>
-                    {/* <button className="fancy-grid-button" disabled={props.pageState.page <= 0} onClick={() => props.pageState.onPageChange(0)}>&lt;&lt;</button> */}
-                    <button className="fancy-grid-button" disabled={props.pageState.page <= 0} onClick={() => props.pageState.onPageChange(props.pageState.page - 1)}>
-                        <i className="far fa-arrow-alt-circle-left"></i>
-                    </button>
-                    <span className="fancy-grid-footer-page-number">Page {props.pageState.page + 1}</span>
-                    <button className="fancy-grid-button" disabled={props.pageState.page >= (props.pageState.numPages - 1)} onClick={() => props.pageState.onPageChange(props.pageState.page + 1)}>
-                    <i className="far fa-arrow-alt-circle-right"></i>
-                    </button>
-                    {/* <button className="fancy-grid-button" disabled={props.pageState.page >= (props.pageState.numPages - 1)} onClick={() => props.pageState.onPageChange(props.pageState.numPages - 1)}>&gt;&gt;</button> */}
-                </td>
-                <td className="fancy-grid-footer-pagesize">
-                    <select className="fancy-grid-select" onChange={(event) => props.pageState.onPageSizeChange(Number(event.target.value), props.pageState.pageSize)} value={props.pageState.pageSize}>
-                        {[5, 10, 20, 50, 100].map(pageSize => (
-                            <option key={pageSize} value={pageSize}>
-                                Show {pageSize}
-                            </option>
-                        ))}
-                    </select> 
-                </td>
-                <td className="fancy-grid-footer-items-showing">
-                    <span className="fancy-grid-footer-items-count">{`${firstItemIndex} - ${lastItemIndex} of ${total} items`}</span>
+                <td colSpan={props.columnDefinitions.length}>
+                    <div style={{width: "100%", display: "flex", flexDirection: "row"}}>
+                        <div style={{flex: "0 0 160px"}}>
+                            <button className="fancy-grid-button" disabled={props.pageState.page <= 0} onClick={() => props.pageState.onPageChange(props.pageState.page - 1)}>
+                                <i className="far fa-arrow-alt-circle-left"></i>
+                            </button>
+                            <span className="fancy-grid-footer-text">Page {props.pageState.page + 1}</span>
+                            <button className="fancy-grid-button" disabled={props.pageState.page >= (props.pageState.numPages - 1)} onClick={() => props.pageState.onPageChange(props.pageState.page + 1)}>
+                            <i className="far fa-arrow-alt-circle-right"></i>
+                            </button>
+                        </div>
+                        <div style={{flex: "0 0 100px"}}>
+                            <select className="fancy-grid-select" onChange={(event) => props.pageState.onPageSizeChange(Number(event.target.value), props.pageState.pageSize)} value={props.pageState.pageSize}>
+                                {[5, 10, 20, 50, 100].map(pageSize => (
+                                    <option key={pageSize} value={pageSize}>
+                                        Show {pageSize}
+                                    </option>
+                                ))}
+                            </select> 
+                        </div>
+                        <div style={{flex: "1 0 0"}}></div>
+                        <div style={{flex: "0 0 160px"}}>
+                            <div className="fancy-grid-footer-text">{`${firstItemIndex} - ${lastItemIndex} of ${total} items`}</div>
+                        </div>
+                    </div>
                 </td>
             </tr>
         </tfoot>
