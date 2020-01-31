8ecfb73953646105c96e41b17d5c54b31970faf3
Sean Price <seanprice@sevenhillstechnology.com>
2020-01-31 14:40:47 -0500

    Upgrade react-bootstrap

--- src/internal-components/FilterButton.tsx
+++ src/internal-components/FilterButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FilterType } from '../models/filterType';
-import {DropdownButton, MenuItem} from 'react-bootstrap';
+import {DropdownButton, Dropdown} from 'react-bootstrap';
 
 export interface DataRowProps {
     filterTypes: FilterType[];
@@ -10,23 +10,21 @@ export interface DataRowProps {
 
 export const FilterButton: React.FunctionComponent<DataRowProps> = props => {
     return (
-        <DropdownButton pullRight className="fancy-grid-filter-button-container"
+        <DropdownButton className="fancy-grid-filter-button-container"
             id="filterSelect"
             title={
                 <div className="filter-button">
                     <i className={`${'filter-button-content'} ${props.value != null ? 'fas fa-filter' : 'far fa-filter'}`} />
                 </div>
-            }
-            noCaret>
+            }>
                 {props.filterTypes.map((filterType, i) => {
                     return(
-                        <MenuItem
-                            eventKey={i}
+                        <Dropdown.Item
                             key={i} 
                             onClick={() => props.onChange(filterType.toString())} 
                             className={`${props.value != null && props.value == filterType ? 'fancy-grid-filter-list-selected' : ''}`}>
                                 {filterType}
-                        </MenuItem>
+                        </Dropdown.Item>
                         )
                     })
                 }
